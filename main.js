2aef4839b1ffe7d9e030d646a4eb816af037a475
ftoledo43 <franciscojaviertoledocollante@gmail.com>
2024-11-17 19:52:01 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+"use strict";
+(() => {
+    class Apocalipsis {
+        constructor(name) {
+            this.name = name;
+        }
+        static callApocalipsis() {
+            if (!Apocalipsis.instance) {
+                Apocalipsis.instance = new Apocalipsis("Soy Apocalipsis el único");
+            }
+            return Apocalipsis.instance;
+        }
+        changeName(newName) {
+            this.name = newName;
+        }
+    }
+    const apocalipsis = Apocalipsis.callApocalipsis();
+    const apocalipsis2 = Apocalipsis.callApocalipsis();
+    const apocalipsis3 = Apocalipsis.callApocalipsis();
+    apocalipsis.changeName("Xavier");
+    console.log(apocalipsis, apocalipsis2, apocalipsis3);
+})();
+//# sourceMappingURL=main.js.map
